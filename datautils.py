c3bbf613a1822229767f4d8870b933049b8bef15
Elias Frantar <elias.frantar@ist.ac.at>
2023-07-20 14:56:41 +0200

    Update datautils.py to fix edge case

--- datautils.py
+++ datautils.py
@@ -75,7 +75,7 @@ def get_c4(nsamples, seed, seqlen, model, tokenizer):
         while True:
             i = random.randint(0, len(traindata) - 1)
             trainenc = tokenizer(traindata[i]['text'], return_tensors='pt')
-            if trainenc.input_ids.shape[1] >= seqlen:
+            if trainenc.input_ids.shape[1] > seqlen:
                 break
         i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
         j = i + seqlen
